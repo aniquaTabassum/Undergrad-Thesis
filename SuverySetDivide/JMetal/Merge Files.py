ef60f22e00527916f42f7721a20ca78ead632457
Aniqua Tabassum <aniquatabassum@aniquas-air.wireless-1x.unc.edu>
2021-04-15 20:50:09 -0400

    found true pareto front

--- SuverySetDivide/JMetal/Merge Files.py
+++ SuverySetDivide/JMetal/Merge Files.py
@@ -1,6 +1,6 @@
-file_to_write = open("/Users/aniquatabassum/Downloads/studies/Undergrad Thesis/SuverySetDivide/JMetal/Results/Merged Results/Merged VAR/Merged Var  setting 4", "w")
-for i in range(160, 210):
-    file1 = open("/Users/aniquatabassum/Downloads/studies/Undergrad Thesis/SuverySetDivide/JMetal/Results/VAR/VAR.NSGAII.Doctor Allocation Trial "+str(i), "r")
+file_to_write = open("/Users/aniquatabassum/Downloads/studies/Undergrad Thesis/SuverySetDivide/JMetal/Results/Merged Results/Merged FUN/Merged FUN Total", "w")
+for i in range(10, 210):
+    file1 = open("/Users/aniquatabassum/Downloads/studies/Undergrad Thesis/SuverySetDivide/JMetal/Results/FUN/FUN.NSGAII.Doctor Allocation Trial "+str(i), "r")
     while True:
         line = file1.readline()
         if not line:
